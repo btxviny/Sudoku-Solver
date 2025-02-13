2ad39bbd0060c4609b3420f2783834de8221129d
Konstantinos Sardelis <Konstantinos.Sardelis@gr.ey.com>
2025-02-13 17:33:32 +0200

    created masks for training

--- create_dataset.py
+++ create_dataset.py
@@ -7,17 +7,16 @@ import time
 from detect_border import detect_border
 
 # Define folders
-image_save_folder = "./data/valid"
+image_save_folder = "./data/images"
 points_save_folder = "./data/vertices"
-non_valid_folder = "./data/non_valid"
 
 # Ensure folders exist
 os.makedirs(image_save_folder, exist_ok=True)
 os.makedirs(points_save_folder, exist_ok=True)
-os.makedirs(non_valid_folder, exist_ok=True)
+
 
 # Load all image files (change the extension if needed)
-files = glob.glob("./data/not_annotated/*.jpeg")  # Modify path accordingly
+files = glob.glob("./data/dataset/images/*.jpeg") + glob.glob("./data/dataset/images/*.jpg")  # Modify path accordingly
 
 # Track saved files for undo functionality
 saved_files = []
@@ -31,9 +30,6 @@ for file in files:
 
     # If no valid Sudoku grid is found, save it in "non_valid"
     if approx is None:
-        non_valid_path = os.path.join(non_valid_folder, f"{base_name}.jpg")
-        cv2.imwrite(non_valid_path, image)
-        print(f"Saved non-valid image for manual annotation: {non_valid_path}")
         continue  # Skip to the next image
     
     # Calculate contour area percentage
@@ -41,20 +37,15 @@ for file in files:
     cv2.putText(image_with_contours, f"{contour_area:.2f}%", (10, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
     # If contour area is less than 40%, move to "non_valid"
     if contour_area < 40:
-        non_valid_path = os.path.join(non_valid_folder, f"{base_name}.jpg")
-        cv2.imwrite(non_valid_path, image)
-        print(f"Contour too small ({contour_area:.2f}%). Moved to non-valid: {non_valid_path}")
         continue  # Skip to the next image
     cv2.imshow('image_with_contours',image_with_contours)
-    time.sleep(1/30)
     # Save valid images automatically
     unique_name = f"{base_name}_{time.time()}"
     image_path = os.path.join(image_save_folder, unique_name + ".jpg")
-    points_path = os.path.join(points_save_folder, unique_name + ".dat")
+    points_path = os.path.join(points_save_folder, unique_name + ".npy")
 
     cv2.imwrite(image_path, image)
-    with open(points_path, "wb") as f:
-        pickle.dump(approx, f)
+    np.save(points_path, approx)
 
     print(f"Saved valid image: {image_path} and points: {points_path}")
     saved_files.append((image_path, points_path))
